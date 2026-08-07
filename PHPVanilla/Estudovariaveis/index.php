354ed9c3e1bf1995753f48d307ec59ef7e7e3105
hannahalmeida678-lang <hannahalmeida678@gmail.com>
2026-08-07 10:54:31 -0300

    dia 07/08/26

--- PHPVanilla/Estudovariaveis/index.php
+++ PHPVanilla/Estudovariaveis/index.php
@@ -1,3 +1,6 @@
+<?php 
+declare(strict_types=1); // blinda o sistema contra misturas acidentais de tipos de dados
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +13,11 @@
    <?php
    // sintaxe para declarar uma variavel em php
    // variaveis são representadas pelo simbolo "$" seguido do nome de uma variavel
+   //exemplo
    $nome = "hannah"; //variavel tipo string
    $idade = 16;// variavel tipo Number
    $status = true; //variavel do tipo boolean
-   $altura = 1.75;//variavel do tipo number(float)
+   $altura = 1.60;//variavel do tipo number(float)
    $email = null; //variavel tipo null
    #$endereço; nao e possivel declarar uma variavel sem atribuir um avlor a ela, não existe Undefined em PHP 
 //exibir as variaveis na tela
@@ -24,11 +28,29 @@
     echo "Email: $email <br>";
 
 echo "<br> <h3> constantes <\h3> <br>";
+
 //constantes são representadas pela palavra "const" ou "define" seguidas do nome da constante
-const PI = 3,14; //constante do tipo number (float)
+const PI = 3.14; //constante do tipo number (float)
 Const EMPRESA = "google"; //constante do tipo string
-define("SITE", "www.google.com");
+define ("SITE", "www.google.com");
 // sempre declarar o nome da constante com letras maiusculas, e o "$" ,com letras minusculas.
+echo "valor de pi: PI <br>";
+echo "nome da empresa: EMPRESA <br.";
+echo "Site: SITE <br>";
+//tentando gerar o valor de uma constante, isso ira gerar um erro, pois constantes não podem ser alteradas
+// PI = 3.14159; // isso e um erro
+//redeclarar uma conatante tambem irá gerar um erro
+  // const SITE = "www.google.com";//isso é um erro
+
+  //Regra de ouro: Sempre coloque a instrução declare (strict_types=1); no inicio do seu codigo php, 
+  //isso blinda o seu sistema contra mistura acidentais de tipo de dados.
+
+  //utilização de textos (concatenação VS interpolaçâo)
+//exemplo de concatenação -> juntar duas ou mais strings utilizando o operador "."
+echo "olá, " . $nome . "! Seja bem-vindo ao nosso site !<br>";
+ //exemplo de interpolação => utilização de variaveis dentro de um texto, utilizando aspas duplas
+ echo "$nome, tem $idade anos e sua altura é $altura metros. <br>"; //forma de misturar texto e variaveis
+
 ?>
 
 
